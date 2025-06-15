10f56c81f8b1c03d7b5ff8fe4cb430f39d5dd125
Ilich8603 <dremin86ilja@gmail.com>
2025-06-15 19:51:11 +0600

    add Parametrized test

--- src/test/java/ru/netology/services/VacationPlaningServicesTest.java
+++ src/test/java/ru/netology/services/VacationPlaningServicesTest.java
@@ -2,36 +2,50 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class VacationPlaningServicesTest {
-    @Test
-    void monthCounterCase1() {
+
+    @ParameterizedTest
+    @CsvFileSource(files="src/test/resources/freelancer_data.csv")
+    public void monthCounter(int income, int expenses, int threshold, int expected) {
         VacationPlaningService service = new VacationPlaningService();
-
-        int income = 10_000;
-        int expenses = 3000;
-        int threshold = 20_000;
-
-        int expected = 3;
-
-        int actual = service.calculate(income, expenses, threshold);
-
-        Assertions.assertEquals(expected, actual);
-
-    }
-
-    @Test
-    void monthCounterCase2() {
-        VacationPlaningService service = new VacationPlaningService();
-
-        int income = 100_000;
-        int expenses = 60_000;
-        int threshold = 150_000;
-
-        int expected = 2;
 
         int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
+
+
+//    @Test
+//    void monthCounterCase1() {
+//        VacationPlaningService service = new VacationPlaningService();
+//
+//        int income = 10_000;
+//        int expenses = 3000;
+//        int threshold = 20_000;
+//
+//        int expected = 3;
+//
+//        int actual = service.calculate(income, expenses, threshold);
+//
+//        Assertions.assertEquals(expected, actual);
+//
+//    }
+//
+//    @Test
+//    void monthCounterCase2() {
+//        VacationPlaningService service = new VacationPlaningService();
+//
+//        int income = 100_000;
+//        int expenses = 60_000;
+//        int threshold = 150_000;
+//
+//        int expected = 2;
+//
+//        int actual = service.calculate(income, expenses, threshold);
+//
+//        Assertions.assertEquals(expected, actual);
+//    }
 }
